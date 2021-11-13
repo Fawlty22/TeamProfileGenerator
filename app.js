caea7a4e58d4a405cf0f503158b0c40b8af5e6fe
Matt Keys <MatthewjKeys22@gmail.com>
2021-11-13 11:46:02 -0700

    added in the begiunnings of prompt recursion

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const { type } = require('os');
-
-
 
 const promptManager = () => {
     console.log('Please build your team:')
@@ -64,7 +61,6 @@ const promptManager = () => {
 }
 
 const promptEmployeeType = managerData => {
-
     return inquirer.prompt([
         {
             type: 'list',
@@ -91,12 +87,141 @@ const promptEmployeeType = managerData => {
 
 }
 
-const promptEngineer = () => {
 
+const promptEngineer = managerData => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'engineerName',
+            message: "What is this engineer's name? (Required)",
+            validate: engineerName => {
+              if (engineerName) {
+                return true;
+              } else {
+                console.log("Please enter this engineer's name!");
+                return false;
+              }
+            }
+          },
+          {
+            type: 'input',
+            name: 'engineerID',
+            message: "What is this engineer's ID #? (Required)",
+            validate: engineerID => {
+              if (engineerID) {
+                return true;
+              } else {
+                console.log("Please enter this engineer's ID #!");
+                return false;
+              }
+            }
+          },
+          {
+            type: 'input',
+            name: 'engineerEmail',
+            message: "What is this engineer's email? (Required)",
+            validate: engineerEmail => {
+              if (engineerEmail) {
+                return true;
+              } else {
+                console.log("Please enter this engineer's email!");
+                return false;
+              }
+            }
+          },
+          {
+            type: 'input',
+            name: 'engineerGithub',
+            message: "What is this engineer's Github username? (Required)",
+            validate: engineerGithub => {
+              if (engineerGithub) {
+                return true;
+              } else {
+                console.log("Please enter this engineer's Github username!");
+                return false;
+              }
+            }
+          },
+    ])
+    .then(engineerData => {
+        if (!managerData.employees){
+            managerData.employees = []
+        };
+
+        managerData.employees.push(engineerData);
+
+        promptEmployeeType(managerData);
+        console.log(managerData)
+
+    })
 }
 
-const promptIntern = () => {
-    
+const promptIntern = managerData => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'internName',
+            message: "What is this intern's name? (Required)",
+            validate: internName => {
+              if (internName) {
+                return true;
+              } else {
+                console.log("Please enter this intern's name!");
+                return false;
+              }
+            }
+          },
+          {
+            type: 'input',
+            name: 'internID',
+            message: "What is this intern's ID #? (Required)",
+            validate: internID => {
+              if (internID) {
+                return true;
+              } else {
+                console.log("Please enter this intern's ID #!");
+                return false;
+              }
+            }
+          },
+          {
+            type: 'input',
+            name: 'internEmail',
+            message: "What is this intern's email? (Required)",
+            validate: internEmail => {
+              if (internEmail) {
+                return true;
+              } else {
+                console.log("Please enter this intern's email!");
+                return false;
+              }
+            }
+          },
+          {
+            type: 'input',
+            name: 'internGithub',
+            message: "What is this intern's Github username? (Required)",
+            validate: internGithub => {
+              if (internGithub) {
+                return true;
+              } else {
+                console.log("Please enter this intern's Github username!");
+                return false;
+              }
+            }
+          },
+    ])
+    .then(internData => {
+        if (!managerData.employees){
+            managerData.employees = []
+        };
+
+        managerData.employees.push(internData);
+        
+        promptEmployeeType(managerData);
+        console.log(managerData)
+    })
+
 }
 
 
