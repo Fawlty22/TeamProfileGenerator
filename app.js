870605eb16e36af7bb3166a93a8d60debb24c574
Matt Keys <MatthewjKeys22@gmail.com>
2021-11-13 11:32:04 -0700

    questions further work

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const { type } = require('os');
 
-const promptUser = () => {
+
+
+const promptManager = () => {
     console.log('Please build your team:')
     return inquirer.prompt([
         //Ask manager questions
@@ -18,7 +21,92 @@ const promptUser = () => {
               }
             }
           },
+          {
+            type: 'input',
+            name: 'managerID',
+            message: "What is the team manager's ID #? (Required)",
+            validate: managerIDInput => {
+              if (managerIDInput) {
+                return true;
+              } else {
+                console.log('Please enter your team managers ID #!');
+                return false;
+              }
+            }
+          },
+          {
+            type: 'input',
+            name: 'managerEmail',
+            message: "What is the team manager's email? (Required)",
+            validate: managerEmailInput => {
+              if (managerEmailInput) {
+                return true;
+              } else {
+                console.log('Please enter your team managers email!');
+                return false;
+              }
+            }
+          },
+          {
+            type: 'input',
+            name: 'managerOffice',
+            message: "What is the team manager's office number? (Required)",
+            validate: managerOfficeInput => {
+              if (managerOfficeInput) {
+                return true;
+              } else {
+                console.log('Please enter your team managers office number!');
+                return false;
+              }
+            }
+          },
     ])
 }
+
+const promptEmployeeType = managerData => {
+
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'employeeType',
+            message: "What kind of team member would you like to add? (Required)",
+            choices: ['Engineer', 'Intern', "I don't want to add anymore team members"],
+            validate: employeeTypeInput => {
+              if (employeeTypeInput) {
+                return true;
+              } else {
+                console.log('Please enter an employee type!');
+                return false;
+              }
+            }
+          },
+    ])
+    .then(typeData => {
+        if (typeData.employeeType === 'Engineer'){
+            promptEngineer(managerData)
+        } else if(typeData.employeeType === 'Intern'){
+            promptIntern(managerData)
+        }
+    })
+
+}
+
+const promptEngineer = () => {
+
+}
+
+const promptIntern = () => {
+    
+}
+
+
+
+
+
+const promptUser = () => {
+    promptManager()
+    .then(promptEmployeeType)
+}
+
 
 promptUser();
